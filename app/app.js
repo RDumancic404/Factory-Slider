566d21ed25049ecbf57be7af0f1529f1239b7cc0
RDumancic404 <rdumancic404@gmail.com>
2021-11-28 13:51:39 +0100

    Initial corrections made

--- app/app.js
+++ app/app.js
@@ -1,69 +1,100 @@
 $(document).ready(function(){
-    $("#rotateRight").click(function(){
-        $("#activeArrows").children().hide();
-        var focusedTop = $("#topRow img:last-child");
-        var focusedBottom = $("#bottomRow img:last-child");
+    $(window).on("resize", function(){location.reload()});
+    var isInAnimation;
 
-        $(".top").animate({
-            left: "+=" + CalculateShiftRight(focusedTop) + "px"
-        }, {queue: false});
-        $(".bottom").animate({
-            left: "+=" + CalculateShiftRight(focusedBottom) + "px"
-        }, {queue: false});
-        $(".rows img:first-child").fadeIn("slow", function(){
-            focusedTop.remove();
-            focusedBottom.remove();
-            $("#activeArrows").children().show();
-        }); 
+    $("#btnLeft").hover( function() {
+        BlueOutButtons($(this));
+    }, function() {
+        GrayOutButtons($(this));
     });
 
-    $("#rotateLeft").click(function(){
-        $("#activeArrows").children().hide();
-        var focusedTop = $("#topRow img:first-child");
-        var focusedBottom = $("#bottomRow img:first-child");
+    $("#btnRight").hover( function() {
+        BlueOutButtons($(this));
+    }, function() {
+        GrayOutButtons($(this));
+    });
 
-        $(".top").animate({
-            left: "-=" + CalculateShiftLeft(focusedTop) + "px"
+    $("#btnRight").click(function(){
+        if(isInAnimation == true) {
+            return;
+        }
+        isInAnimation = true;
+        var focusedTop = $("#topRow img:last-child");
+        var focusedBottom = $("#bottomRow img:last-child");
+        LoopRight(focusedTop, focusedBottom);
+
+        let anim1 = $(".top").animate({
+            left: "+=" + CalculateShift(focusedTop) + "px"
         }, {queue: false});
-        $(".bottom").animate({
-            left: "-=" + CalculateShiftLeft(focusedBottom) + "px"
+        let anim2 = $(".bottom").animate({
+            left: "+=" + CalculateShift(focusedBottom) + "px"
         }, {queue: false});
-        $(".rows img:last-child").fadeIn("slow", function(){
+        let anim3 = $(".rows img:first-child").fadeIn("slow");
+        $.when(anim1, anim2, anim3).then(function() {
             focusedTop.remove();
             focusedBottom.remove();
-            $("#activeArrows").children().show();
+            isInAnimation = false;
+        });
+    });
+
+    $("#btnLeft").click(function(){
+        if(isInAnimation == true) {
+            return;
+        }
+        isInAnimation = true;
+        var focusedTop = $("#topRow img:first-child");
+        var focusedBottom = $("#bottomRow img:first-child");
+        LoopLeft(focusedTop, focusedBottom);
+
+        let anim1 = $(".top").animate({
+            left: "-=" + CalculateShift(focusedTop) + "px"
+        }, {queue: false});
+        let anim2 = $(".bottom").animate({
+            left: "-=" + CalculateShift(focusedBottom) + "px"
+        }, {queue: false});
+        let anim3 = $(".rows img:first-child").fadeOut("slow");
+        $.when(anim1, anim2, anim3).then(function() {
+            focusedTop.remove();
+            focusedBottom.remove();
+            isInAnimation = false;
         }); 
     });
 });
 
-function CalculateShiftRight(focused){
-    var shiftBy = focused.width();
-    LoopRight(focused);
-    // Added +10 for the borders between images
-    return shiftBy + 10;
-}
-
-function CalculateShiftLeft(focused){
-    var shiftBy = focused.width();
-    LoopLeft(focused);
-    // Added +10 for the borders between images
-    return shiftBy + 10;
-}
-
-function LoopRight(focused){
-    if(focused.hasClass("top")){
-        focused.clone().prependTo("#topRow").hide()
-    } else if(focused.hasClass("bottom")){
-        focused.clone().prependTo("#bottomRow").hide()
-    } 
-    $(".rows img:first-child").css( "left", "0")
-}
-
-function LoopLeft(focused){
-    if(focused.hasClass("top")){
-        focused.clone().appendTo("#topRow").hide()
-    } else if(focused.hasClass("bottom")){
-        focused.clone().appendTo("#bottomRow").hide()
+function GrayOutButtons(button) {
+    $(button).css("border-color", "#DDDDDD");
+    if(button.attr("id") == "btnRight") {
+        button.children("img:first").attr("src", "assets/arrow-gray-right.png")
+    } else {
+        button.children("img:first").attr("src", "assets/arrow-gray-left.png")
     }
-    $(".rows img:last-child").css( "right", "0").css("left", "auto")
+}
+
+function BlueOutButtons(button) {
+    $(button).css("border-color", "#134880");
+    if(button.attr("id") == "btnRight") {
+        button.children("img:first").attr("src", "assets/arrow-blue-right.png")
+    } else {
+        button.children("img:first").attr("src", "assets/arrow-blue-left.png")
+    }
+}
+
+function CalculateShift(focused){
+    var shiftBy = focused.width();
+    // Added +10 for the borders between images
+    return shiftBy + 10;
+}
+
+function LoopRight(top, bottom){
+    top.clone().prependTo("#topRow");
+    bottom.clone().prependTo("#bottomRow");
+    $("#topRow img:first-child").css("right", "1240px").css( "left", "auto");
+    $("#bottomRow img:first-child").css("right", "1090px").css( "left", "auto");
+}
+
+function LoopLeft(top, bottom){
+    top.clone().appendTo("#topRow");
+    bottom.clone().appendTo("#bottomRow");
+    $("#topRow img:last-child").css( "right", "-" + CalculateShift(top) + "px").css("left", "auto");
+    $("#bottomRow img:last-child").css( "right", "-" + CalculateShift(bottom) + "px").css("left", "auto");
 }
